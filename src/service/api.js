a40dedb3ae6709140dd3a20a0e39bc1174f73737
Rudramani7 <rudramani1410@gmail.com>
2022-03-18 10:27:50 +0530

    commit

--- src/service/api.js
+++ src/service/api.js
@@ -4,7 +4,7 @@ const URL = 'https://news-b-clone.herokuapp.com/';
 
 export const getNews = async (page, size = 5) => {
     try {
-        return await axios.get(`${URL}/news?page=${page}&size=${size}`);
+        return await axios.get(`${URL}news?page=${page}&size=${size}`);
     } catch (error) {
         console.log('Error while calling getNews API',error);
     }
